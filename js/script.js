3c59d02c1dbc19ef2626c293fc3ca0bdb0c5f49b
Mucteba Fatih Unal <m_fattih@hotmail.com>
2021-02-24 11:23:03 -0800

    added form control

--- js/script.js
+++ js/script.js
@@ -1,44 +1,51 @@
 let longURL = document.getElementById("longurl");
 let shortURL = document.getElementById("shorturl");
+let cleanBtn = document.getElementById("cleanBtn");
+
+//let axiosURL = "http://localhost:5000";
 let axiosURL = "https://m-url.herokuapp.com";
 
-longURL.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(longURL.value);
-});
-
 function submitForm() {
-  console.log(longURL.value);
-  try {
-    axios
-      .post(axiosURL + "/api/url", {
-        longUrl: longURL.value,
-      })
-      .then((response) => {
-        console.log(response.data.url);
-        shortURL.value = response.data.url.shortUrl;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  } catch (err) {
-    console.error(err);
+  if (longURL.value) {
+    longURL.className = "form-control";
+    cleanBtn.className = "input-group-text";
+
+    try {
+      axios
+        .post(axiosURL + "/api/url", {
+          longUrl: longURL.value,
+        })
+        .then((response) => {
+          //console.log(response.data.url);
+          shortURL.value = response.data.url.shortUrl;
+        })
+        .catch((error) => {
+          console.log(error);
+          longURL.className = "form-control is-invalid";
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    console.log("noldu");
+    longURL.className = "form-control is-invalid";
   }
 }
 
 function copyClipboard() {
-  console.log("copyde");
   shortURL.select();
   document.execCommand("copy");
 }
-
+function clearForm() {
+  longURL.value = "";
+  shortURL.value = "";
+  cleanBtn.className = "input-group-text isShow";
+}
 function wakeUpServer() {
   try {
     axios
       .get(axiosURL)
-      .then((response) => {
-        console.log(response);
-      })
+      .then(() => {})
       .catch((error) => {
         console.log(error);
       });
